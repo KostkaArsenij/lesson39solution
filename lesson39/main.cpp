19d71d33f6a816cf8108a25549dd37a6341b990b
Kostka-A <misterg378@gmail.com>
2025-02-11 17:45:11 +0300

    Add project files.

---
+++ lesson39/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "util.h"
+#include <ctime>
+
+using namespace  std;
+#define SIZE 20
+
+int main() {
+	srand(time(NULL));
+	int array[SIZE];
+
+	init(array, SIZE, -20, 20);
+
+	cout << "Array: " << convert(array, SIZE) << endl;
+
+	return 0;
+}
