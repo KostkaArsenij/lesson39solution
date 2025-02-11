eecb77b2c14fb9f397acffb0c68139c5939e5a87
Kostka-A <misterg378@gmail.com>
2025-02-11 17:46:14 +0300

    update

--- lesson39/logic.cpp
+++ lesson39/logic.cpp
@@ -9,6 +9,7 @@ string convert(int* array, int size) {
 	}
 	return s;
 }
+
 void init(int* array, int size, int a, int b) {
 	for (int i = 0; i < size; i++)
 	{
--- lesson39/main.cpp
+++ lesson39/main.cpp
@@ -6,12 +6,7 @@ using namespace  std;
 #define SIZE 20
 
 int main() {
-	srand(time(NULL));
-	int array[SIZE];
-
-	init(array, SIZE, -20, 20);
-
-	cout << "Array: " << convert(array, SIZE) << endl;
+	
 
 	return 0;
 }
